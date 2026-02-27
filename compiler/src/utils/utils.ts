a31b883b854de7f40fa1694561dc1d3e4f1cc5e0
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-02-27 11:00:56 +0100

    feat: moved to packages/*

--- compiler/src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export function stripQuotes(value: string): string {
-    if (
-        (value.startsWith("'") && value.endsWith("'")) ||
-        (value.startsWith('"') && value.endsWith('"'))
-    ) {
-        return value.slice(1, -1);
-    }
-    return value;
-}
