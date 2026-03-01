2f9a2734245e0bb6daf8e4be2e7d95744179472e
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-01 07:41:05 +0100

    feat: added constants

---
+++ packages/core/framework/core/constants.ts
@@ -0,0 +1,4 @@
+export const COMPONENT_VARIABLE = '%COMP%';
+export const HOST_ATTR = `_nghost-${COMPONENT_VARIABLE}`;
+export const CONTENT_ATTR = `_ngcontent-${COMPONENT_VARIABLE}`;
+export const SVG_NS = "http://www.w3.org/2000/svg";
